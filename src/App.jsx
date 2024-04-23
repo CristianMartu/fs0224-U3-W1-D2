cceb2c4da819322b058e222d5fdd22c36e5ac372
CristianMartu <martuccicristian18@gmail.com>
2024-04-23 17:51:11 +0200

    change render for category

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import logo from './logo.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import MyNavbar from './components/MyNavbar'
+import MyNav from './components/MyNav'
+import MyFooter from './components/MyFooter'
+import Welcome from './components/Welcome'
+import AllTheBooks from './components/AllTheBooks'
 
 function App() {
   return (
     <div className="App">
-      <MyNavbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <MyNav name="EpiBooks" />
+      {/* <Welcome /> */}
+      <AllTheBooks />
+      <MyFooter />
     </div>
   )
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+// import { Alert } from 'bootstrap'
+
+import { Container } from 'react-bootstrap'
+
+const Welcome = () => <Container className="alert alert-primary p-3 my-2">A simple primary alert—check it out!</Container>
+
+export default Welcome
